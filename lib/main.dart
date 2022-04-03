1a49f8135c411ab54aa2d9ec1742ef1396eaf2bd
Ram kaushik komanduri <ramkaushikkomanduri@Rams-MacBook-Pro.local>
2022-04-03 16:39:15 +0530

    Day3  completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/HomePage.dart';
 import 'package:flutter_application_1/pages/Login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 void main()
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.purple),
+      // fontFamily: GoogleFonts.lato().fontFamily,
 
       //darkTheme: ThemeData(brightness: Brightness.dark),//for dark mode chnage thememode to dark and uncomment this
 
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class LoginPage extends StatelessWidget {
@@ -7,16 +10,71 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assests/images/login_page.png",fit: BoxFit.cover),
+
+          SizedBox(
+            height: 40.0,
           ),
+
+          Text(
+            "Welcome",
+            style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            fontFamily: GoogleFonts.lato().fontFamily,
+          ),
+          ),
+
+          SizedBox(
+            height: 30.0,
+          ),
+          
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32.0,horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter mail id",
+                    labelText: "Mail Id"
+                  ),
+                ),
+
+
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Phone Number",
+                    labelText: "Phone Number"
+                  ),
+                ),
+
+                TextFormField(
+                  obscureText:true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Passowrd",
+                    labelText: "Password"
+                  ),
+                ),
+
+
+                SizedBox(
+                height: 20.0,
+                ),
+
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Register")),
+                  
+              ],
+            ),
+          )
+
+        ],
       )
-      ),
       
     );
   }
