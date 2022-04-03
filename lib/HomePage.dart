009e013b8ef523beb33755595ca3535c9e36e0d6
Ram kaushik komanduri <ramkaushikkomanduri@Rams-MacBook-Pro.local>
2022-04-03 14:18:51 +0530

    Day1

---
+++ lib/HomePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  final int hours = 8;
+  final String name = "flutter";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("1st trail"),
+      ),
+      body: Center(
+      child: Container(
+        child: Text("welcome to  $hours hrs of $name"),
+      ),
+    ),
+    drawer: Drawer(backgroundColor: Colors.black12),
+    );
+  }
+}
