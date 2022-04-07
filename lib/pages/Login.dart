670d4afe609894d817b05f6b9243304046975d0e
Ram kaushik komanduri <ramkaushikkomanduri@Rams-MacBook-Pro.local>
2022-04-07 16:17:44 +0530

    Day 6 completed form&validation

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -16,117 +16,176 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changed = false;
+  final _formkey = GlobalKey<FormState>(); 
+
+  moveToHome(BuildContext context) async
+  {
+    if(_formkey.currentState!.validate())
+    {
+      setState(() {
+        changed = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context,MyRoutes.homeRoute);
+      setState(() {
+        changed = false;
+      });
+    }
+    
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assests/images/login_page.png",fit: BoxFit.cover),
-      
-            SizedBox(
-              height: 40.0,
-            ),
-      
-            Text(
-              "Welcome $name",
-              style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              fontFamily: GoogleFonts.lato().fontFamily,
-            ),
-            ),
-      
-            SizedBox(
-              height: 30.0,
-            ),
-            
-      
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32.0,horizontal: 32.0),
-              child: Column(
-                children: [
-
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter name",
-                      labelText: "Username"
-                    ),
-                    onChanged: (value){
-                      name = value;
-                      setState(() {
-                        
-                      });
-                    },
-                  ),
-
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter mail id",
-                      labelText: "Mail Id"
-                    ),
-                  ),
-      
-      
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Phone Number",
-                      labelText: "Phone Number"
-                    ),
-                  ),
-      
-                  TextFormField(
-                    obscureText:true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Passowrd",
-                      labelText: "Password"
-                    ),
-                  ),
-      
-      
-                  SizedBox(
-                  height: 20.0,
-                  ),
-      
-                  InkWell(
-                    onTap: () async{
-                      setState(() {
-                        changed = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-
-                    },
-                    child: AnimatedContainer(
-                      
-                      duration: Duration(seconds: 1),
-                      width: changed?50:150,
-                      height: 40,
-                      
-                      alignment: Alignment.center,
-                      child: changed?Icon(Icons.done,color: Colors.white,):Text(
-                        "Register",
-                        style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.white),
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.purple,
-                        borderRadius: BorderRadius.circular(changed?50:10)
-                      ), 
-                    ),
-                  )
-
-                  // ElevatedButton(
-                  //   onPressed: (){
-                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  //   },//Navigator.push(context, MaterialPageRoute(builder: (context => Homepage()),)),
-                  //   child: Text("Register")),
-                    
-                ],
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.asset("assests/images/login_page.png",fit: BoxFit.cover),
+              
+              SizedBox(
+                height: 40.0,
               ),
-            )
-      
-          ],
+              
+              Text(
+                "Welcome $name",
+                style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontFamily: GoogleFonts.lato().fontFamily,
+              ),
+              ),
+              
+              SizedBox(
+                height: 30.0,
+              ),
+              
+              
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 32.0,horizontal: 32.0),
+                
+                  child: Column(
+                    children: [
+                
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter name",
+                          labelText: "Username"
+                        ),
+                        validator: (value){
+                          if(value!.isEmpty)
+                          {
+                            return "Username cannot be empty";
+                          }
+                          else
+                            return null;
+                        },
+                        onChanged: (value){
+                          name = value;
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter mail id",
+                          labelText: "Mail Id"
+                        ),
+                        validator: (value){
+                          if(value!.isEmpty)
+                          {
+                            return "Email is compulsory";
+                          }
+                          else
+                          {
+                            return null;
+                          }
+                        },
+                      ),
+                      
+                      
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Phone Number",
+                          labelText: "Phone Number"
+                        ),
+                        validator: (value)
+                        {
+                          if(value!.isEmpty)
+                          {
+                            return "Phone Number cannot be empty";
+                          }
+                          else
+                          {
+                            return null;
+                          }
+                        },
+                      ),
+                      
+                      TextFormField(
+                        obscureText:true,
+                        decoration: InputDecoration(
+                          hintText: "Enter Passowrd",
+                          labelText: "Password"
+                        ),
+                        validator: (value)
+                        {
+                          if(value!.isEmpty)
+                          {
+                            return "Passowrd is compulsory";
+                          }
+                          else
+                          {
+                            return null;
+                          }
+                        },
+                      ),
+                      
+                      
+                      SizedBox(
+                      height: 20.0,
+                      ),
+                      
+                      Material(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(changed?50:10),
+                        child: InkWell(
+                          onTap: () =>moveToHome(context),
+                          child: AnimatedContainer(
+                            
+                            duration: Duration(seconds: 1),
+                            width: changed?50:150,
+                            height: 40,
+                            
+                            alignment: Alignment.center,
+                            child: changed?Icon(Icons.done,color: Colors.white,):Text(
+                              "Register",
+                              style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.white),
+                            ),
+                            
+                          ),
+                        ),
+                      )
+                
+                      // ElevatedButton(
+                      //   onPressed: (){
+                      //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      //   },//Navigator.push(context, MaterialPageRoute(builder: (context => Homepage()),)),
+                      //   child: Text("Register")),
+                        
+                    ],
+                  ),
+                ),
+              
+              
+            ],
+          ),
         ),
       )
       
